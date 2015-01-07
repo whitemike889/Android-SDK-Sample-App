101d6ba707e84117ad6142bf7b23d0785bdd3190
Franklin <franklin.michael@payu.in>
2015-01-07 15:12:13 +0530

    netbanking with app's UI

--- app/src/main/java/com/payu/sample/MainActivity.java
+++ app/src/main/java/com/payu/sample/MainActivity.java
@@ -1,5 +1,6 @@
 package com.payu.sample;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.v7.app.ActionBarActivity;
@@ -7,65 +8,89 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.EditText;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 import com.payu.sdk.Constants;
+import com.payu.sdk.GetResponseTask;
 import com.payu.sdk.Params;
 import com.payu.sdk.PayU;
 import com.payu.sdk.Payment;
+import com.payu.sdk.PaymentListener;
 import com.payu.sdk.ProcessPaymentActivity;
+import com.payu.sdk.adapters.NetBankingAdapter;
 import com.payu.sdk.exceptions.HashException;
 import com.payu.sdk.exceptions.MissingParameterException;
 
+import org.apache.http.NameValuePair;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-public class MainActivity extends ActionBarActivity {
+import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
+import java.util.List;
 
-    String cardNumber;
-    String expiryMonth;
-    String expiryYear;
-    String nameOnCard;
-    String cvv;
+
+public class MainActivity extends ActionBarActivity implements PaymentListener{
+
     Payment payment;
     Payment.Builder builder = new Payment().new Builder();
+    Params requiredParams = new Params();
+
+    ProgressDialog mProgressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mProgressDialog = new ProgressDialog(this);
+        mProgressDialog.setMessage(getString(com.payu.sdk.R.string.please_wait));
+        mProgressDialog.setIndeterminate(true);
+        mProgressDialog.setCancelable(false);
+        mProgressDialog.show();
+
         builder.set(PayU.PRODUCT_INFO, "product");
         builder.set(PayU.AMOUNT, "5.0");
         builder.set(PayU.TXNID, "mytxn1");
         builder.set(PayU.SURL, "http://www.google.com");
-        builder.set(PayU.MODE, String.valueOf(PayU.PaymentMode.CC));
+        builder.set(PayU.MODE, String.valueOf(PayU.PaymentMode.NB));
 
-        findViewById(R.id.makePayment).setOnClickListener(new View.OnClickListener() {
+        requiredParams.put(PayU.AMOUNT, builder.get(PayU.AMOUNT));
+        requiredParams.put(PayU.PRODUCT_INFO, builder.get(PayU.PRODUCT_INFO));
+        requiredParams.put(PayU.TXNID, builder.get(PayU.TXNID));
+        requiredParams.put(PayU.SURL, builder.get(PayU.SURL));
+
+
+        List<NameValuePair> postParams = null;
+
+        HashMap<String, String> varList = new HashMap<String, String>();
+        varList.put(Constants.VAR1, Constants.DEFAULT);
+
+        try {
+
+            Bundle bundle = getPackageManager().getApplicationInfo(getPackageName(), PackageManager.GET_META_DATA).metaData;
+            requiredParams.put(PayU.MERCHANT_KEY, bundle.getString("payu_merchant_id"));
+            payment = builder.create();
+
+            postParams = PayU.getInstance(this).getParams(Constants.GET_IBIBO_CODES, varList);
+            GetResponseTask getResponse = new GetResponseTask((com.payu.sdk.PaymentListener) this);
+            getResponse.execute(postParams);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        } catch (MissingParameterException e) {
+            e.printStackTrace();
+        }
+
+        ((ListView)findViewById(R.id.netBankingListView)).setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onClick(View view) {
-                Params requiredParams = new Params();
-
-                cardNumber = String.valueOf(((EditText)findViewById(R.id.cardNumberEditText)).getText());
-                expiryMonth = String.valueOf(((EditText)findViewById(R.id.expiryMonth)).getText());
-                expiryYear = String.valueOf(((EditText)findViewById(R.id.expiryYear)).getText());
-                nameOnCard = String.valueOf(((EditText)findViewById(R.id.nameOnCardEditText)).getText());
-                cvv = String.valueOf(((EditText)findViewById(R.id.cvvEditText)).getText());
-
-                requiredParams.put(PayU.CARD_NUMBER, cardNumber);
-                requiredParams.put(PayU.EXPIRY_MONTH, String.valueOf(expiryMonth));
-                requiredParams.put(PayU.EXPIRY_YEAR, String.valueOf(expiryYear));
-                requiredParams.put(PayU.CARDHOLDER_NAME, nameOnCard);
-                requiredParams.put(PayU.CVV, cvv);
-
-                requiredParams.put(PayU.AMOUNT, builder.get(PayU.AMOUNT));
-                requiredParams.put(PayU.PRODUCT_INFO, builder.get(PayU.PRODUCT_INFO));
-                requiredParams.put(PayU.TXNID, builder.get(PayU.TXNID));
-                requiredParams.put(PayU.SURL, builder.get(PayU.SURL));
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
                 try {
-                    Bundle bundle = getPackageManager().getApplicationInfo(getPackageName(), PackageManager.GET_META_DATA).metaData;
-
-                    requiredParams.put(PayU.MERCHANT_KEY, bundle.getString("payu_merchant_id"));
-                    payment = builder.create();
+                    requiredParams.put(PayU.BANKCODE, ((JSONObject) parent.getAdapter().getItem(position)).getString("code"));
 
                     String postData = PayU.getInstance(MainActivity.this).createPayment(payment, requiredParams);
 
@@ -79,10 +104,9 @@ public class MainActivity extends ActionBarActivity {
                     e.printStackTrace();
                 } catch (HashException e) {
                     e.printStackTrace();
-                } catch (PackageManager.NameNotFoundException e) {
+                } catch (JSONException e) {
                     e.printStackTrace();
                 }
-
             }
         });
 
@@ -109,5 +133,24 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onPaymentOptionSelected(PayU.PaymentMode paymentMode) {
+
+    }
+
+    @Override
+    public void onGetAvailableBanks(JSONArray response) {
+        NetBankingAdapter netBankingAdapter= new NetBankingAdapter(this, PayU.availableBanks);
+        ((ListView)findViewById(R.id.netBankingListView)).setAdapter(netBankingAdapter);
+        if(mProgressDialog != null);
+        mProgressDialog.dismiss();
+
+    }
+
+    @Override
+    public void onGetStoreCardDetails(JSONArray response) {
+
     }
 }
