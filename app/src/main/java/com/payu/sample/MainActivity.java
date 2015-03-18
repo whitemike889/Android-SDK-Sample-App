d6c66c20b389764ad80f25d4a6d6e5968d4a9857
Franklin <franklin.michael@payu.in>
2015-03-18 13:57:07 +0530

    basic flow updated for verson 2.0 and cb version 4.0

--- app/src/main/java/com/payu/sample/MainActivity.java
+++ app/src/main/java/com/payu/sample/MainActivity.java
@@ -1,183 +1,187 @@
 package com.payu.sample;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.support.v7.app.ActionBarActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.payu.sdk.Constants;
-import com.payu.sdk.GetResponseTask;
-import com.payu.sdk.Params;
 import com.payu.sdk.PayU;
-import com.payu.sdk.Payment;
-import com.payu.sdk.PaymentListener;
-import com.payu.sdk.ProcessPaymentActivity;
-import com.payu.sdk.exceptions.HashException;
-import com.payu.sdk.exceptions.MissingParameterException;
 
 import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
 import org.json.JSONException;
+import org.json.JSONObject;
 
-import java.security.NoSuchAlgorithmException;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
 
-public class MainActivity extends ActionBarActivity implements PaymentListener{
+public class MainActivity extends ActionBarActivity {
 
-    String cardNumber;
-    String expiryMonth;
-    String expiryYear;
-    String nameOnCard;
-    String cvv;
-    Payment payment;
-    Payment.Builder builder = new Payment().new Builder();
+    Intent intent;
+    ProgressDialog mProgressDialog;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        if(Constants.ENABLE_VAS){
-            if(Constants.ENABLE_VAS && PayU.issuingBankDownBin == null){ // vas has not been called, lets fetch bank down time.
-                HashMap<String, String> varList = new HashMap<String, String>();
+        mProgressDialog = new ProgressDialog(MainActivity.this);
 
-                varList.put("var1", "default");
-                varList.put("var2", "default");
-                varList.put("var3", "default");
+        // create sample progress dialog.
 
-                List<NameValuePair> postParams = null;
+//        textView = findViewById(R.id.tv1);
 
-                try {
-                    postParams = PayU.getInstance(this).getParams(Constants.GET_VAS, varList);
-                    GetResponseTask getResponse = new GetResponseTask(MainActivity.this);
-                    getResponse.execute(postParams);
-                } catch (NoSuchAlgorithmException e) {
-                    e.printStackTrace();
-                }
-            }
+        if(savedInstanceState == null) {
+            setContentView(R.layout.activity_main);
         }
 
-        builder.set(PayU.PRODUCT_INFO, "product");
-        builder.set(PayU.AMOUNT, "5.0");
-        builder.set(PayU.TXNID, String.valueOf("Production :" + System.currentTimeMillis()));
-        builder.set(PayU.SURL, "http://www.google.com");
-        builder.set(PayU.MODE, String.valueOf(PayU.PaymentMode.CC));
-
-        findViewById(R.id.makePayment).setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.add).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Params requiredParams = new Params();
-
-                cardNumber = String.valueOf(((EditText)findViewById(R.id.cardNumberEditText)).getText());
-                expiryMonth = String.valueOf(((EditText)findViewById(R.id.expiryMonth)).getText());
-                expiryYear = String.valueOf(((EditText)findViewById(R.id.expiryYear)).getText());
-                nameOnCard = String.valueOf(((EditText)findViewById(R.id.nameOnCardEditText)).getText());
-                cvv = String.valueOf(((EditText)findViewById(R.id.cvvEditText)).getText());
-
-                requiredParams.put(PayU.CARD_NUMBER, cardNumber);
-                requiredParams.put(PayU.EXPIRY_MONTH, String.valueOf(expiryMonth));
-                requiredParams.put(PayU.EXPIRY_YEAR, String.valueOf(expiryYear));
-                requiredParams.put(PayU.CARDHOLDER_NAME, nameOnCard);
-                requiredParams.put(PayU.CVV, cvv);
-
-                requiredParams.put(PayU.AMOUNT, builder.get(PayU.AMOUNT));
-                requiredParams.put(PayU.PRODUCT_INFO, builder.get(PayU.PRODUCT_INFO));
-                requiredParams.put(PayU.TXNID, builder.get(PayU.TXNID));
-                requiredParams.put(PayU.SURL, builder.get(PayU.SURL));
-
-                try {
-                    Bundle bundle = getPackageManager().getApplicationInfo(getPackageName(), PackageManager.GET_META_DATA).metaData;
-
-                    requiredParams.put(PayU.MERCHANT_KEY, bundle.getString("payu_merchant_id"));
-                    payment = builder.create();
-
-                    String postData = PayU.getInstance(MainActivity.this).createPayment(payment, requiredParams);
-
-                    Intent intent = new Intent(MainActivity.this, ProcessPaymentActivity.class);
-                    intent.putExtra(Constants.POST_DATA, postData);
-
-                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-
-                    startActivityForResult(intent, PayU.RESULT);
-                } catch (MissingParameterException e) {
-                    e.printStackTrace();
-                } catch (HashException e) {
-                    e.printStackTrace();
-                } catch (PackageManager.NameNotFoundException e) {
-                    e.printStackTrace();
-                }
-
+                LinearLayout linearLayout = (LinearLayout) findViewById(R.id.params);
+                linearLayout.addView(getLayoutInflater().inflate(R.layout.param, null), linearLayout.getChildCount() - 2);
             }
         });
 
-        ((EditText)findViewById(R.id.cardNumberEditText)).addTextChangedListener(new TextWatcher() {
+        findViewById(R.id.done).setOnClickListener(new View.OnClickListener() {
             @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                if(s.length() > 7 && PayU.issuingBankDownBin != null && PayU.issuingBankDownBin.has(s.toString().substring(0, 6))){
-                    try {
-                        ((TextView)findViewById(R.id.errorMessage)).setText("We are experiencing high failure rate for " + PayU.issuingBankDownBin.getString(s.toString().substring(0, 6)) + " cards at this time. We recommend you pay using any other means of payment.");
-                        findViewById(R.id.errorMessage).setVisibility(View.VISIBLE);
-                    } catch (JSONException e) {
-                        e.printStackTrace();
+            public void onClick(View view) {
+                LinearLayout linearLayout = (LinearLayout) findViewById(R.id.params);
+                final HashMap<String, String> params = new HashMap<String, String>();
+                double amount = 10;
+                for (int i = 0; i < linearLayout.getChildCount() - 2; i++) {
+                    LinearLayout param = (LinearLayout) linearLayout.getChildAt(i);
+                    if(((TextView) param.getChildAt(0)).getText().toString().equals("amount")) {
+                        amount = Double.valueOf(((EditText) param.getChildAt(1)).getText().toString());
                     }
-
-                }else{
-                    findViewById(R.id.errorMessage).setVisibility(View.INVISIBLE);
+                    params.put(((TextView) param.getChildAt(0)).getText().toString(), ((EditText) param.getChildAt(1)).getText().toString());
                 }
-            }
+//                String hash = calculateHash(params);
+                params.remove("amount");
+//                params.put("hash", hash);
 
-            @Override
-            public void afterTextChanged(Editable s) {
+                if(!Constants.SDK_HASH_GENERATION){ // cool we gotta fetch hash from the server.
+
+                    mProgressDialog.setCancelable(false);
+                    mProgressDialog.setMessage("Calculating Hash. please wait..");
+                    mProgressDialog.show();
+
+                    final double finalAmount = amount;
+                    new AsyncTask<Void, Void, Void>(){
+                        @Override
+                        protected Void doInBackground(Void... voids){
+                            try {
+                                HttpClient httpclient = new DefaultHttpClient();
+                                HttpPost httppost = new HttpPost(Constants.FETCH_DATA_URL);
+                                List<NameValuePair> postParams = new ArrayList<NameValuePair>(5);
+                                postParams.add(new BasicNameValuePair("command", "mobileHashTestWs"));
+                                postParams.add(new BasicNameValuePair("key", (getPackageManager().getApplicationInfo(getPackageName(), PackageManager.GET_META_DATA).metaData).getString("payu_merchant_id")));
+                                postParams.add(new BasicNameValuePair("var1", params.get("txnid")));
+                                postParams.add(new BasicNameValuePair("var2", String.valueOf(finalAmount)));
+                                postParams.add(new BasicNameValuePair("var3", params.get("productinfo")));
+                                postParams.add(new BasicNameValuePair("var4", params.get("user_credentials")));
+                                postParams.add(new BasicNameValuePair("hash", "helo"));
+                                httppost.setEntity(new UrlEncodedFormEntity(postParams));
+                                JSONObject response = new JSONObject(EntityUtils.toString(httpclient.execute(httppost).getEntity()));
+
+
+                                // set the hash values here.
+
+                                if(response.has("result") ){
+                                    PayU.merchantCodesHash = response.getJSONObject("result").getString("merchantCodesHash");
+                                    PayU.paymentHash = response.getJSONObject("result").getString("paymentHash");
+                                    PayU.vasHash = response.getJSONObject("result").getString("mobileSdk");
+                                    PayU.ibiboCodeHash = response.getJSONObject("result").getString("detailsForMobileSdk");
+
+                                    if(response.getJSONObject("result").has("deleteHash")){
+                                        PayU.deleteCardHash = response.getJSONObject("result").getString("deleteHash");
+                                        PayU.getUserCardHash = response.getJSONObject("result").getString("getUserCardHash");
+                                        PayU.editUserCardHash = response.getJSONObject("result").getString("editUserCardHash");
+                                        PayU.saveUserCardHash = response.getJSONObject("result").getString("saveUserCardHash");
+                                    }
+
+                                }
+                                mProgressDialog.dismiss();
+
+                                PayU.getInstance(MainActivity.this).startPaymentProcess(finalAmount, params);
+
+                            } catch (UnsupportedEncodingException e) {
+                                e.printStackTrace();
+                                /*if(mProgressDialog.isShowing())
+                                    mProgressDialog.dismiss();*/
+                                Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
+                            } catch (ClientProtocolException e) {
+                                e.printStackTrace();
+                                /*if(mProgressDialog.isShowing())
+                                    mProgressDialog.dismiss();*/
+                                Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
+                            } catch (JSONException e) {
+                                if(mProgressDialog.isShowing())
+                                    mProgressDialog.dismiss();
+                                e.printStackTrace();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                                /*if(mProgressDialog.isShowing())
+                                    mProgressDialog.dismiss();*/
+                                Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
+                            } catch (PackageManager.NameNotFoundException e) {
+                                e.printStackTrace();
+                                /*if(mProgressDialog.isShowing())
+                                    mProgressDialog.dismiss();*/
+                                Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
+                            }
+                            return null;
+                        }
+                    }.execute();
+                }else{
+                    PayU.getInstance(MainActivity.this).startPaymentProcess(amount, params);
+                }
+
+
 
             }
         });
 
+        ((EditText) findViewById(R.id.txn)).setText(String.valueOf(System.currentTimeMillis()));
+
+        String version = "version 2.1 sms";
+
+        Toast.makeText(MainActivity.this, Constants.DEBUG ? "Debug build " + version   : "Production build " + version, Toast.LENGTH_SHORT).show();
+        Toast.makeText(MainActivity.this, Constants.SDK_HASH_GENERATION ? "SDK is generating Hash " : "SDK fetches hash form API", Toast.LENGTH_SHORT).show();
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == PayU.RESULT) {
+            if(resultCode == RESULT_OK) {
+                //success
+                if(data != null )
+                    Toast.makeText(this, "Success" + data.getStringExtra("result"), Toast.LENGTH_LONG).show();
+            }
+            if (resultCode == RESULT_CANCELED) {
+                //failed
+                if(data != null)
+                    Toast.makeText(this, "Failed" + data.getStringExtra("result"), Toast.LENGTH_LONG).show();
+            }
         }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public void onPaymentOptionSelected(PayU.PaymentMode paymentMode) {
-
-    }
-
-    @Override
-    public void onGetResponse(String responseMessage) {
-        Toast.makeText(this, responseMessage, Toast.LENGTH_SHORT).show();
     }
 }
