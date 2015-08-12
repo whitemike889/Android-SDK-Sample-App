111c67215701563113faeebd2e6f3328f869a1de
FranklinMichael <franklin@FranklinMichael.local>
2015-08-12 18:25:25 +0530

    updating to sdk v4.1 and cb version 5.1

---
+++ sdkui/src/main/src/com/payu/payuui/PaymentsActivity.java
@@ -0,0 +1,256 @@
+package com.payu.payuui;
+
+import android.app.AlertDialog;
+import android.content.BroadcastReceiver;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import com.payu.custombrowser.Bank;
+import com.payu.custombrowser.PayUWebChromeClient;
+import com.payu.custombrowser.PayUWebViewClient;
+import com.payu.india.Extras.PayUSdkDetails;
+import com.payu.india.Model.PayuConfig;
+import com.payu.india.Payu.PayuConstants;
+
+import org.apache.http.util.EncodingUtils;
+
+public class PaymentsActivity extends AppCompatActivity{
+
+    Bundle bundle;
+    WebView mWebView;
+    String url;
+    boolean cancelTransaction = false;
+
+    PayuConfig payuConfig;
+    private BroadcastReceiver mReceiver = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        /**
+         * when the device runing out of memory we dont want the user to restart the payment. rather we close it and redirect them to previous activity.
+         */
+        if(savedInstanceState!=null){
+            super.onCreate(null);
+            finish();//call activity u want to as activity is being destroyed it is restarted
+        }else {
+            super.onCreate(savedInstanceState);
+        }
+        setContentView(R.layout.activity_payments);
+
+        //region Replace the whole code by the commented code if you are NOT using custombrowser
+        // Replace the whole code by the commented code if you are NOT using custombrowser.
+
+        /*bundle = getIntent().getExtras();
+        payuConfig = bundle.getParcelable(PayuConstants.PAYU_CONFIG);
+
+        mWebView = (WebView) findViewById(R.id.webview);
+
+        url = payuConfig.getEnvironment() == PayuConstants.PRODUCTION_ENV?  PayuConstants.PRODUCTION_PAYMENT_URL : PayuConstants.MOBILE_TEST_PAYMENT_URL ;
+
+        byte[] encodedData = EncodingUtils.getBytes(payuConfig.getData(), "base64");
+        mWebView.postUrl(url, encodedData);
+
+
+        mWebView.getSettings().setSupportMultipleWindows(true);
+        mWebView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.getSettings().setDomStorageEnabled(true);
+        mWebView.setWebChromeClient(new WebChromeClient() {});
+        mWebView.setWebViewClient(new WebViewClient() {});*/
+        //endregion
+
+        bundle = getIntent().getExtras();
+        payuConfig = bundle.getParcelable(PayuConstants.PAYU_CONFIG);
+
+        mWebView = (WebView) findViewById(R.id.webview);
+        mWebView.getSettings().setCacheMode(WebSettings.LOAD_NO_CACHE);
+
+        try {
+            Class.forName("com.payu.custombrowser.Bank");
+            final Bank bank = new Bank() {
+                @Override
+                public void registerBroadcast(BroadcastReceiver broadcastReceiver, IntentFilter filter) {
+                    mReceiver = broadcastReceiver;
+                    registerReceiver(broadcastReceiver, filter);
+                }
+
+                @Override
+                public void unregisterBroadcast(BroadcastReceiver broadcastReceiver) {
+                    if(mReceiver != null){
+                        unregisterReceiver(mReceiver);
+                        mReceiver = null;
+                    }
+                }
+
+                @Override
+                public void onHelpUnavailable() {
+                    findViewById(R.id.parent).setVisibility(View.GONE);
+                    findViewById(R.id.trans_overlay).setVisibility(View.GONE);
+                }
+
+                @Override
+                public void onBankError() {
+                    findViewById(R.id.parent).setVisibility(View.GONE);
+                    findViewById(R.id.trans_overlay).setVisibility(View.GONE);
+                }
+
+                @Override
+                public void onHelpAvailable() {
+                    findViewById(R.id.parent).setVisibility(View.VISIBLE);
+                }
+            };
+            Bundle args = new Bundle();
+            args.putInt("webView", R.id.webview);
+            args.putInt("tranLayout",R.id.trans_overlay);
+            args.putInt("mainLayout",R.id.r_layout);
+
+            String [] list =  payuConfig.getData().split("&");
+            String txnId = null;
+            String merchantKey = null;
+            for (String item : list) {
+                if(item.contains("txnid")){
+                    txnId = item.split("=")[1];
+                }else if (item.contains("key")){
+                    merchantKey = item.split("=")[1];
+                }
+                if (null != txnId && null != merchantKey) break;
+            }
+            args.putString(Bank.TXN_ID, txnId == null ? String.valueOf(System.currentTimeMillis()) : txnId);
+            args.putString(Bank.MERCHANT_KEY, null != merchantKey ? merchantKey : "could not find");
+            PayUSdkDetails payUSdkDetails = new PayUSdkDetails();
+            args.putString(Bank.SDK_DETAILS, "VersionCode: " + payUSdkDetails.getSdkVersionCode() + ", VersionName: " + payUSdkDetails.getSdkVersionName());
+            if(getIntent().getExtras().containsKey("showCustom")) {
+                args.putBoolean("showCustom", getIntent().getBooleanExtra("showCustom", false));
+            }
+            args.putBoolean("showCustom", true);
+            bank.setArguments(args);
+            findViewById(R.id.parent).bringToFront();
+            try {
+                getSupportFragmentManager().beginTransaction().setCustomAnimations(R.anim.fade_in, R.anim.cb_face_out).add(R.id.parent, bank).commit();
+            }catch(Exception e)
+            {
+                e.printStackTrace();
+                finish();
+            }
+            mWebView.setWebChromeClient(new PayUWebChromeClient(bank));
+            mWebView.setWebViewClient(new PayUWebViewClient(bank));
+        } catch (ClassNotFoundException e) {
+            mWebView.getSettings().setSupportMultipleWindows(true);
+            mWebView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+            mWebView.addJavascriptInterface(new Object() {
+                @JavascriptInterface
+                public void onSuccess() {
+                    onSuccess("");
+                }
+
+                @JavascriptInterface
+                public void onSuccess(final String result) {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Intent intent = new Intent();
+                            intent.putExtra("result", result);
+                            setResult(RESULT_OK, intent);
+                            finish();
+                        }
+//                }
+                    });
+                }
+
+                @JavascriptInterface
+                public void onFailure() {
+                    onFailure("");
+                }
+
+                @JavascriptInterface
+                public void onFailure(final String result) {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Intent intent = new Intent();
+                            intent.putExtra("result", result);
+                            setResult(RESULT_CANCELED, intent);
+                            finish();
+                        }
+                    });
+                }
+            }, "PayU");
+
+            mWebView.setWebChromeClient(new WebChromeClient() {
+
+            });
+            mWebView.setWebViewClient(new WebViewClient());
+        }
+
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.getSettings().setDomStorageEnabled(true);
+        url = payuConfig.getEnvironment() == PayuConstants.PRODUCTION_ENV?  PayuConstants.PRODUCTION_PAYMENT_URL : PayuConstants.MOBILE_TEST_PAYMENT_URL ;
+        mWebView.postUrl(url, EncodingUtils.getBytes(payuConfig.getData(), "base64"));
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_payments, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed(){
+        if(cancelTransaction){
+            cancelTransaction = false;
+            Intent intent = new Intent();
+            intent.putExtra("result", "Transaction canceled due to back pressed!");
+            setResult(RESULT_CANCELED, intent);
+            super.onBackPressed();
+            return;
+        }
+
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);;
+        alertDialog.setCancelable(false);
+        alertDialog.setMessage("Do you really want to cancel the transaction ?");
+        alertDialog.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                cancelTransaction = true;
+                dialog.dismiss();
+                onBackPressed();
+            }
+        });
+        alertDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        alertDialog.show();
+    }
+}
